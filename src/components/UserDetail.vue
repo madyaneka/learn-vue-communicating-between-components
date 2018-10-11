bb3fe93ea107ed656db02bde8c539f57e8408d06
Madyan Eka <madyan.eka95@gmail.com>
2018-10-11 22:17:45 +0700

    parent to child component (refined)

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,15 +2,21 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User name: {{ name }}</p>
+        <p>User name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
 export default {
     props: [
-        'name'
-    ]
+        'myName'
+    ],
+
+    methods: {
+        switchName() {
+            return this.myName.split('').reverse().join('')
+        }
+    }
 }
 </script>
 
